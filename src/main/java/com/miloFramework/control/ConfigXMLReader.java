990c50e82de269e6c5c0035983b19da4dbd3a6e8
523944754@qq.com <523944754@qq.com>
2016-07-06 17:37:00 +0800

    删除多余代码

--- src/main/java/com/miloFramework/control/ConfigXMLReader.java
+++ src/main/java/com/miloFramework/control/ConfigXMLReader.java
@@ -25,35 +25,7 @@ public class ConfigXMLReader {
 	    
 		public static final String module = ConfigXMLReader.class.getName();
 	    public static final String controllerXmlFileName = "/WEB-INF/controller.xml";
-	    private static final Map<String, Class<?>> primitiveMap = FastMap.newInstance();
-	    
-	    
-	    {
-
-	    	registered("long",long.class);
-	    	registered("boolean", boolean.class);
-	    	registered("char", char.class);
-	    	registered("int",int.class);
-	    	registered("double",double.class);
-	    	registered("float",float.class);
-	    	registered("byte", byte.class);
-	    	registered("short", short.class);	   
-	    	registered("Integer",Integer.class);
-	    	registered("Float",Float.class);
-	    	registered("Double",Double.class);
-	    	registered("character",Character.class);
-	    	registered("Long",Long.class);
-	    	registered("Boolean",Boolean.class);
-	    	registered("Byte", Byte.class);
-	    	registered("Short", Short.class);
-	    	registered("date",Date.class);
-	    	registered("string",String.class);
-	    	registered("object",Object.class);
-	    }
-	    
-	    private static void registered(String className,Class<?> clazz){
-	    	primitiveMap.put(className, clazz);
-	    }
+	
 	    
 	    
 	    public static URL getControllerConfigURL(ServletContext context) {
